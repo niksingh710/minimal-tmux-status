df1aea10c89bf7f66512cd4ab60ca2e367d03911
niksingh710 <nik.singh710@gmail.com>
2024-06-03 23:12:14 +0530

    Added support for nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+{pkgs ? import <nixpkgs> {}}:
+pkgs.tmuxPlugins.mkTmuxPlugin {
+  pluginName = "minimal-tmux-status";
+  version = "1.0";
+  src = ./.;
+  rtpFilePath = "minimal.tmux";
+
+  meta = {
+    homepage = "https://github.com/niksingh710/minimal-tmux-status";
+    description = "Minimal Status Bar";
+    license = pkgs.lib.licenses.mit;
+    platforms = pkgs.lib.platforms.unix;
+    maintainers = with pkgs.lib.maintainers; [niksingh710];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = (import ./default.nix { inherit pkgs; });
+  };
+}
